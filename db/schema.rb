8ae0da1d86bff668f80e264e4981e593c87061a2
felipe-bitt <felipebittfernandes@gmail.com>
2024-10-11 01:01:19 -0300

    add Restaurant class and create Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_07_165033) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_11_025033) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "menu_items", force: :cascade do |t|
-    t.bigint "menu_id", null: false
     t.string "name"
     t.decimal "price"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["menu_id"], name: "index_menu_items_on_menu_id"
+  end
+
+  create_table "menu_items_menus", id: false, force: :cascade do |t|
+    t.bigint "menu_item_id", null: false
+    t.bigint "menu_id", null: false
+    t.index ["menu_id"], name: "index_menu_items_menus_on_menu_id"
+    t.index ["menu_item_id"], name: "index_menu_items_menus_on_menu_item_id"
   end
 
   create_table "menus", force: :cascade do |t|
@@ -29,7 +34,17 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_07_165033) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "restaurant_id", null: false
+    t.index ["restaurant_id"], name: "index_menus_on_restaurant_id"
   end
 
-  add_foreign_key "menu_items", "menus"
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "menus", "restaurants"
 end
