669e0783453b861393c5bc125ba0b282f7576a41
felipe-bitt <felipebittfernandes@gmail.com>
2024-10-11 01:05:05 -0300

    menu_items controller adjustment

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,21 +1,54 @@
-class MenuItemsController < ApplicationRecord
-  before_action :set_menu
+class MenuItemsController < ApplicationController
+  before_action :set_menu, only: [:index, :show, :create, :update, :destroy]
+  before_action :set_menu_item, only: [:show, :update, :destroy]
 
   def index
-    menu_items = @menu.menu_items
-
-    render json: menu_items
+    render json: @menu.menu_items, status: :ok
   end
 
   def show
-    menu_item = @menu.menu_item.find(params[:id])
+    render json: @menu_item, status: :ok
+  end
 
-    render json: menu_item
+  def create
+    menu_item = @menu.menu_items.build(menu_item_params)
+
+    if menu_item.save
+      render json: menu_item, status: :created
+    else
+      render json: { errors: menu_item.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @menu_item.update(menu_item_params)
+      render json: @menu_item, status: :ok
+    else
+      render json: { errors: @menu_item.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @menu_item.destroy
+
+    head :no_content
   end
 
   private
 
   def set_menu
     @menu = Menu.find(params[:menu_id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Menu not found' }, status: :not_found
+  end
+
+  def set_menu_item
+    @menu_item = @menu.menu_items.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'Menu item not found' }, status: :not_found
+  end
+
+  def menu_item_params
+    params.require(:menu_item).permit(:name, :price, :description)
   end
 end
