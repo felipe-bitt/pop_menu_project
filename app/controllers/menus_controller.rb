8a85c653dbb1fd608efd7f248e7760948d021c3b
felipe-bitt <felipebittfernandes@gmail.com>
2024-10-11 01:05:16 -0300

    menus_controller adjustment

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,13 +1,54 @@
-class MenusController < ApplicationRecord
-  def index
-    menus = Menu.all
+class MenusController < ApplicationController
+  before_action :set_restaurant, only: [:index, :create]
+  before_action :set_menu, only: [:show, :update, :destroy]
 
-    render json: menus
+  def index
+    @menus = @restaurant.menus
+
+    render json: @menus, include: :menu_items
   end
 
   def show
-    menu = Menu.find_(params[:id])
+    render json: @menu, include: :menu_items
+  end
 
-    render json: menu, include: :menu_items
+  def create
+    @menu = @restaurant.menus.build(menu_params)
+
+    if @menu.save
+      render json: @menu, status: :created
+    else
+      render json: @menu.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @menu.update(menu_params)
+      render json: @menu
+    else
+      render json: @menu.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @menu.destroy
+
+    head :no_content
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def set_menu
+    @menu = Menu.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Menu not found" }, status: :not_found
+  end
+
+  def menu_params
+    params.require(:menu).permit(:name, :description, menu_item_ids: [])
   end
 end
