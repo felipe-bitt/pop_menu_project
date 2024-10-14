3e57e6ee211897cb69563722d0d26f72b7fac42a
felipe-bitt <felipebittfernandes@gmail.com>
2024-10-14 10:32:18 -0300

    improved json_imports_controller structure

--- app/controllers/json_imports_controller.rb
+++ app/controllers/json_imports_controller.rb
@@ -1,26 +1,25 @@
 class JsonImportsController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
+  class JsonImportError < StandardError; end 
+
   def create
-    if params[:file].present?
-      begin
-        file = params[:file].read
-        json_data = JSON.parse(file)
+    return render json: { error: 'No file was uploaded' }, status: :unprocessable_entity unless params[:file].present?
 
-        result = JsonImportService.new(json_data).process
+    begin
+      file = params[:file].read
+      json_data = JSON.parse(file)
 
-        if result[:success]
-          render json: { message: 'File successfully imported', logs: result[:logs] }, status: :ok
-        else
-          render json: { message: 'File import failed', logs: result[:logs] }, status: :unprocessable_entity
-        end
-      rescue JSON::ParserError
-        render json: { error: 'Invalid JSON format' }, status: :unprocessable_entity
-      rescue StandardError => e
-        render json: { error: "An error occurred: #{e.message}" }, status: :internal_server_error
-      end
-    else
-      render json: { error: 'No file was uploaded' }, status: :unprocessable_entity
+
+      result = JsonImportService.new(json_data).process
+
+      return render json: { message: 'File import failed', logs: result[:logs] }, status: :unprocessable_entity unless result[:success]
+
+      render json: { message: 'File successfully imported', logs: result[:logs] }, status: :ok
+    rescue JSON::ParserError
+      render json: { error: 'Invalid JSON format' }, status: :unprocessable_entity
+    rescue JsonImportError => e
+      render json: { error: "An error occurred: #{e.message}" }, status: :internal_server_error
     end
   end
 end
